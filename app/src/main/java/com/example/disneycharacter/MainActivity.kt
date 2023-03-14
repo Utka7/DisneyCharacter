31d0e45394aa3160dddb3e118d1793b6a65e11f1
Your Name <you@example.com>
2023-03-14 12:52:43 +0300

    added toolbar

--- app/src/main/java/com/example/disneycharacter/MainActivity.kt
+++ app/src/main/java/com/example/disneycharacter/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.disneycharacter
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
     }
 }
